7531f283fbe93edab2a50a1427e329816dae1d66
shulgastanslv <shulgastanslv@gmail.com>
2024-05-29 09:38:03 +0300

    fix: handle delete

--- app/(home)/_components/navbar/logo.tsx
+++ app/(home)/_components/navbar/logo.tsx
@@ -1,28 +1,34 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { endMeet, getMeetByUserId } from "@/services/meet-service";
 import { useSearchParams } from "next/navigation";
 import { getSelf } from "@/services/session-service";
 import { deleteMeet } from "@/actions/meet";
+import { Button } from "react-day-picker";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
-export const Logo = async () => {
+export const Logo = () => {
 
-    async function deleteMt () {
-        await deleteMeet()
-    }
+  const [isPending, startTransition] = useTransition();
 
-    return (
-        <Link href="/" onClick={deleteMt}>
-            <div className="flex items-center marker:hover:opacity-75 transition">
-                <div className="rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
-                        <Image
-                            src='/logo.svg'
-                            alt="CorpNetCast"
-                            height="23"
-                            width="23"
-                        />
-                </div>
-            </div>
-        </Link>
-    );
+  const handleClick = (e: any) => {
+    startTransition(() => {
+      deleteMeet()
+        .then(() => {
+          toast.success("Звонок завершен!");
+        })
+    });
+  };
+
+  return (
+    <Link href="/" onClick={(e) => handleClick(e)}>
+      <div className="flex items-center marker:hover:opacity-75 transition">
+        <div className="rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+          <Image src="/logo.svg" alt="CorpNetCast" height="23" width="23" />
+        </div>
+      </div>
+    </Link>
+  );
 };
